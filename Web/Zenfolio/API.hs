3f8a8c126fc6cca5aa4a15d868bde6ad9d1a6191
Mike Dillon <mike@embody.org>
2010-05-26 01:38:53 -0700

    Trim exports of Web.Zenfolio.API

--- Web/Zenfolio/API.hs
+++ Web/Zenfolio/API.hs
@@ -1,21 +1,15 @@
 module Web.Zenfolio.API (
-    module Network.JsonRpc,
     module Control.Monad.Trans,
 
     module Web.Zenfolio.Monad,
     module Web.Zenfolio.Types,
-    module Web.Zenfolio.Utils,
     module Web.Zenfolio.Auth
 ) where
 
 import Control.Monad.Trans (liftIO)
 
-import Network.JsonRpc (RpcAction, rpcEnv, runRpc)
-
 import Web.Zenfolio.Monad
 
 import Web.Zenfolio.Types
-
-import Web.Zenfolio.Utils
 
 import Web.Zenfolio.Auth
