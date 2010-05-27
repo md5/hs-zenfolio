b4f8b6e40033fc27382131a1c48d32eac3e92911
Mike Dillon <mike@embody.org>
2010-05-26 18:57:44 -0700

    Move write helpers to Network.JsonRpc.Utils
    Add oJSONField for fields that should be omitted from JSON structs when they
    are Nothing

--- Network/JsonRpc/Utils.hs
+++ Network/JsonRpc/Utils.hs
@@ -1,9 +1,14 @@
 module Network.JsonRpc.Utils (
     field,
     mField,
-    lField
+    lField,
+
+    mShowJSON,
+    oJSONField,
+    recTypeField
 ) where
 
+import Data.Data (Data, toConstr, showConstr)
 import Text.JSON (JSON(..), JSObject(..), JSValue(..), Result)
 
 -- Normal field, directly deserialized
@@ -12,7 +17,7 @@ field k obj = maybe (fail $ "No such element: " ++ k)
                     readJSON
                     (lookup k (fromJSObject obj))
 
--- Nullable field with null translated to Maybe and value wrapped in Just
+-- Nullable field with null translated to Nothing and value wrapped in Just
 mField :: JSON a => String -> JSObject JSValue -> Result (Maybe a)
 mField k obj = maybe (fail $ "No such element: " ++ k)
                      (\val -> case val of
@@ -27,3 +32,13 @@ lField k obj = maybe (fail $ "No such element: " ++ k)
                                    JSNull -> return []
                                    _      -> readJSON val)
                      (lookup k (fromJSObject obj))
+
+mShowJSON :: JSON a => Maybe a -> JSValue
+mShowJSON = maybe JSNull showJSON
+
+oJSONField :: JSON a => String -> Maybe a -> Maybe (String, JSValue)
+oJSONField f (Just value) = Just (f, showJSON value)
+oJSONField _ Nothing      = Nothing
+
+recTypeField :: Data a => a -> (String,JSValue)
+recTypeField d = ("$type", showJSON $ showConstr (toConstr d))
