55ae3bb6f96c2778564e934a8305876264e56c62
Mike Dillon <mike@embody.org>
2010-06-06 15:28:49 -0700

    Add AuthenticatePlain

--- Web/Zenfolio/Auth.hs
+++ Web/Zenfolio/Auth.hs
@@ -1,13 +1,15 @@
 module Web.Zenfolio.Auth (
     getChallenge,
     authenticate,
+    authenticatePlain,
+
     login
 ) where
 
 import qualified Data.Digest.SHA256 as SHA256 (hash)
 import Data.String.UTF8 (fromString, toRep)
 import Web.Zenfolio.Monad (ZM)
-import Web.Zenfolio.RPC (zfRemote)
+import Web.Zenfolio.RPC (zfRemote, zfRemoteSsl)
 import Web.Zenfolio.Types (LoginName, AuthChallenge(..), Password, AuthToken)
 
 getChallenge :: LoginName -> ZM AuthChallenge
@@ -21,6 +23,9 @@ authenticate challenge password = do
           passwordBytes  = toRep $ fromString password
           roundOneBytes  = SHA256.hash $ saltBytes      ++ passwordBytes
           roundTwoBytes  = SHA256.hash $ challengeBytes ++ roundOneBytes
+
+authenticatePlain :: LoginName -> Password -> ZM AuthToken
+authenticatePlain = zfRemoteSsl "AuthenticatePlain"
 
 login :: LoginName -> Password -> ZM AuthToken
 login username password = do
