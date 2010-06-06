c5f37d0642769a9f1b385724285f2670c63d9478
Mike Dillon <mike@embody.org>
2010-06-06 15:29:36 -0700

    Add a bunch of photoset methods

--- Web/Zenfolio/PhotoSets.hs
+++ Web/Zenfolio/PhotoSets.hs
@@ -1,14 +1,51 @@
 module Web.Zenfolio.PhotoSets (
     createPhotoSet,
-    loadPhotoSet
+    deletePhotoSet,
+    loadPhotoSet,
+    movePhotoSet,
+    reorderPhotoSet,
+    setPhotoSetTitlePhoto,
+    setPhotoSetFeaturedIndex,
+    updatePhotoSet,
+
+    newUpdater
 ) where
 
 import Web.Zenfolio.Monad (ZM)
 import Web.Zenfolio.RPC (zfRemote)
-import Web.Zenfolio.Types (GroupID, PhotoSet, PhotoSetID, PhotoSetType, PhotoSetUpdater)
+import Web.Zenfolio.Types (GroupID, GroupIndex,
+                           PhotoSet, PhotoSetID, PhotoSetType, PhotoSetUpdater(..),
+                           PhotoID, ShiftOrder)
 
 createPhotoSet :: GroupID -> PhotoSetType -> PhotoSetUpdater -> ZM PhotoSet
 createPhotoSet = zfRemote "CreatePhotoSet"
 
+deletePhotoSet :: PhotoSetID -> ZM ()
+deletePhotoSet = zfRemote "DeletePhotoSet"
+
 loadPhotoSet :: PhotoSetID -> ZM PhotoSet
 loadPhotoSet = zfRemote "LoadPhotoSet"
+
+movePhotoSet :: PhotoSetID -> GroupID -> GroupIndex -> ZM ()
+movePhotoSet = zfRemote "MovePhotoSet"
+
+reorderPhotoSet :: PhotoSetID -> ShiftOrder -> ZM ()
+reorderPhotoSet = zfRemote "ReorderPhotoSet"
+
+setPhotoSetFeaturedIndex :: PhotoSetID -> GroupIndex -> ZM ()
+setPhotoSetFeaturedIndex = zfRemote "SetPhotoSetFeaturedIndex"
+
+setPhotoSetTitlePhoto :: PhotoSetID -> PhotoID -> ZM ()
+setPhotoSetTitlePhoto = zfRemote "SetPhotoSetTitlePhoto"
+
+updatePhotoSet :: PhotoSetID -> PhotoSetUpdater -> ZM PhotoSet
+updatePhotoSet = zfRemote "UpdatePhotoSet"
+
+newUpdater :: PhotoSetUpdater
+newUpdater = PhotoSetUpdater {
+                 psuTitle = Nothing,
+                 psuCaption = Nothing,
+                 psuKeywords = Nothing,
+                 psuCategories = Nothing,
+                 psuCustomReference = Nothing
+             }
